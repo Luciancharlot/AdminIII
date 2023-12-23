0a7fd13e7fe9b50c08b53730014695fabc4b5f51
Lucian Charlot <91597965+Luciancharlot@users.noreply.github.com>
2023-12-23 15:52:30 +0100

    Update CreateUser.ps1

--- CreateUser.ps1
+++ CreateUser.ps1
@@ -1,10 +1,12 @@
 $userData = Import-Csv 'C:\Users\Administrator\Desktop\utilisateurs.csv'
 
 foreach ($user in $userData) {
-    $firstname = $user.'Prénom'
+    $firstname = $user.'Prenom'
     $lastname = $user.'Nom'
-    $username = $user.'Nom Utilisateur'
+    $username = $user.'NomUtilisateur'
+    $completename = $user.'Complet'
+    $password $vser = $user.'password'
 
-    New-ADUser -SamAccountName $username -UserPrincipalName "$username@grp34.lab" -GivenName $firstname -Surname $lastname -Enabled $true
+    New-ADUser -SamAccountName $username -UserPrincipalName "$username@grp34.lab" -GivenName $firstname -Surname $lastname -Name $lastname -AccountPassword (convertTo.SecureString $password -AsPlainText -Force) -Enabled $true
     Write-Host "Utilisateur $username créé avec succès."
 }
